ee0dced328836b7f4a46a80de6929ea01bd0bbcd
YoussefHamadaYH <youssefarqam772@gmail.com>
2024-09-18 23:39:37 +0300

    #Create a TypeComponent to display all types with functionalities to view, add, or delete individual types.

--- src/app/components/type/type.component.ts
+++ src/app/components/type/type.component.ts
@@ -12,17 +12,39 @@ import { CommonModule } from '@angular/common';
   styleUrl: './type.component.css'
 })
 export class TypeComponent implements OnInit {
-Types: IType[]=[];
-  constructor(public TypeServ:TypeService) {}
+  Types: IType[] = [];
+
+  constructor(public TypeServ: TypeService) {}
+
   ngOnInit(): void {
     this.TypeServ.GetAllTypes().subscribe({
-      next: (response) =>
-        this.Types = response
+      next: (response) => (this.Types = response),
     });
   }
+
   deleteHandler(typeId: any) {
     this.TypeServ.DeleteType(typeId).subscribe({
-      next: () => { this.Types = this.Types.filter((t=>t.typeId!=typeId)); }
+      next: () => {
+        this.Types = this.Types.filter((t) => t.typeId != typeId);
+      },
     });
+  }
+
+  hoverIn() {
+    var btn1 = document.getElementById('btn1');
+    var btn2 = document.getElementById('btn2');
+    if (btn1 && btn2) {
+      btn1.style.display = 'none';
+      btn2.style.display = 'block';
+    }
+  }
+
+  hoverOut() {
+    var btn1 = document.getElementById('btn1');
+    var btn2 = document.getElementById('btn2');
+    if (btn1 && btn2) {
+      btn1.style.display = 'block';
+      btn2.style.display = 'none';
+    }
   }
 }
