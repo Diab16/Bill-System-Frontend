d37f4d7c41dea2ba90709e296821daaeb2cb865c
YoussefHamadaYH <youssefarqam772@gmail.com>
2024-09-18 23:40:58 +0300

    #Create a Type_Details component to view the details of an individual type.

--- src/app/components/type-details/type-details.component.ts
+++ src/app/components/type-details/type-details.component.ts
@@ -13,7 +13,7 @@ import { IType } from '../../Models/iType';
 })
 export class TypeDetailsComponent implements OnInit {
   typeId: number = 0;
-  Type: IType | null = null; // Use the IType interface
+  Type: IType | null = null;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -22,14 +22,10 @@ export class TypeDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Get the typeId from the URL
     this.typeId = Number(this.activatedRoute.snapshot.params['id']);
-
-    // Fetch the type details using the service
     this.TypeServ.GetTypeById(this.typeId).subscribe({
       next: (response: IType) => {
         this.Type = response;
-        console.log(this.Type);
       },
       error: (err) => {
         console.error('Error fetching type details:', err);
@@ -37,18 +33,7 @@ export class TypeDetailsComponent implements OnInit {
     });
   }
 
-  editType() {
-    // Navigate to the edit form with the typeId
-    this.router.navigate([`/manage/editType/${this.typeId}`]);
-  }
-
-  addType() {
-    // Navigate to the add type form
-    this.router.navigate(['/manage/addType']);
-  }
-
   backToProducts() {
-    // Navigate back to the types list
-    this.router.navigate(['/Type']);
+    this.router.navigate(['/manage/Type']);
   }
 }
