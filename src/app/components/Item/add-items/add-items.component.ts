feb5b0fa175ea1041432171e6db280c01141ca29
Alaa Sleem <alaa.sleem9090@gmail.com>
2024-09-25 18:14:45 +0300

    Adjust add item and edit item component

--- src/app/components/Item/add-items/add-items.component.ts
+++ src/app/components/Item/add-items/add-items.component.ts
@@ -6,7 +6,6 @@ import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router'
 import { ItemsServiceService } from '../../../Services/items-service.service';
 import { IFormdata } from '../../../Interfaces/Iformdata';
 import { catchError, map, Observable, of } from 'rxjs';
-import { TypeService } from '../../../Services/type.service';
 
 @Component({
   selector: 'app-add-items',
@@ -20,27 +19,48 @@ export class AddItemsComponent implements OnInit {
   isLoading:boolean=false;
   formdata!: IFormdata; 
   itemms:Iitems[] = [];
-  typeList: { id: number; name: string }[] = []; 
   companyList: { id: number; name: string  ,types:{id:number , name:string}[] }[] = []; 
   unitsList: { id: number; name: string }[] = []; 
   successMessage: string | null = null; // For storing success message
   selectedCompany: { id: number; name: string; types:{id :number , name:string}[]} | null = null;
 
-  constructor( private service:ItemsServiceService , public router:Router,private typeService:TypeService){}
+  constructor( private service:ItemsServiceService , public router:Router){}
   AddItemsForm:FormGroup = new FormGroup({
     companyId: new FormControl("" , [Validators.required]),
     typeId:new FormControl('' , [Validators.required,]),
     name: new FormControl('', {
       validators: [Validators.required],
       asyncValidators: [this.uniqueItemNameValidator()],
-      updateOn: 'change' 
+      updateOn: 'blur' 
     }),
     sellingPrice:new FormControl(null , [ Validators.required , Validators.min(0)]),
     buyingPrice:new FormControl(null , [ Validators.required , Validators.min(0)] ),
     availableAmount:new FormControl(null , [ Validators.required , Validators.min(0)] ),
     unitId:new FormControl('',[Validators.required]),
     notes:new FormControl('')
-  },{validators:[this.PriceValidation(),this.chooseCompanyFirst()]} );
+  },{validators:this.PriceValidation()} );
+
+  ngOnInit(): void {
+    this.service.getFormData().subscribe({
+      next: (response) => {
+        this.formdata = response;
+        this.companyList = this.formdata.companies;
+        this.unitsList = this.formdata.units;
+      },
+      error: (error) => {
+        console.error('Error fetching form data:', error);
+      }
+    });
+  }
+
+  //event to get the types of the selected company 
+
+  onCompanyChange(event: any) {
+    const companyId = +event.target.value;
+    this.selectedCompany = this.companyList.find(company => company.id === companyId) || null;
+    this.AddItemsForm.patchValue({ typeId: '' }); // Reset typeId when company changes
+  }
+
 
    //price custom  validators
     PriceValidation():ValidatorFn {
@@ -58,16 +78,6 @@ export class AddItemsComponent implements OnInit {
    }
 
   }
-  //Choose Company First
-  chooseCompanyFirst(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const compId = control.get('companyId')?.value;
-      if( compId == ''){
-        return {chooseCompanyFirst: true} ;
-      }
-      return null;
-    };
-  }
    //unique VValidator
   uniqueItemNameValidator(): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
@@ -78,47 +88,12 @@ export class AddItemsComponent implements OnInit {
       return this.service.getAllItems().pipe(
         map(items => {
           this.itemms = items; 
-          const itemNameExists = this.itemms.some(item => item.name.toLowerCase() === control.value.toLowerCase());
+          const itemNameExists = this.itemms.some(item => item.name === control.value);
           return itemNameExists ? { uniqueItemName: true } : null;
         }),
         catchError(() => of(null)) 
       );
     };
-  }
-
-  ngOnInit(): void {
-    this.AddItemsForm.get('companyId')?.valueChanges.subscribe(() => this.getCompanyTypes());
-    this.service.getFormData().subscribe({
-      next: (response) => {
-        this.formdata = response;
-        this.companyList = this.formdata.companies;
-        //this.typeList = this.formdata.types;
-        this.unitsList = this.formdata.units;
-      },
-      error: (error) => {
-        console.error('Error fetching form data:', error);
-      }
-    });
-  }
-  getCompanyTypes(){
-    this.typeList = [];
-    const companyName = this.companyList.find(c=>c.id == this.AddItemsForm.get('companyId')?.value)?.name;
-    console.log(companyName);
-    
-    this.typeService.GetTypeByCompanyName(companyName).subscribe({
-      next:(response)=>{
-        for (var type of response) {
-          var mappedType = {
-            id:type.typeId,
-            name:type.typeName
-          }
-          this.typeList.push(mappedType);
-        }
-        this.AddItemsForm.get('typeId')?.reset('');
-        console.log(this.typeList);
-        
-      }
-    })
   }
 
 
@@ -139,6 +114,7 @@ export class AddItemsComponent implements OnInit {
          }, 1500);
       } , error: (error) => {
         console.error('Error fetching form data:', error);
+        this.showMessage("Error Adding Item !!!");
       }
     })
     this.isLoading=false; 
@@ -156,6 +132,13 @@ export class AddItemsComponent implements OnInit {
   }
 
  }
+
+ showMessage(message: string) {
+  this.Message = message;
+  setTimeout(() => {
+    this.Message =''; 
+  }, 2000); 
+}
 
 
 }
